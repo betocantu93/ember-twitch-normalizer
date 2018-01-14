091865ec0c135baa9d661a44410066297bab5e10
Alberto Cantú Gómez <betocantu93@gmail.com>
2018-01-14 04:55:04 -0600

    modified for better understanding

--- addon/adapters/application.js
+++ addon/adapters/application.js
@@ -4,7 +4,7 @@ export default DS.JSONAPIAdapter.extend({
   host: 'https://api.twitch.tv',
   namespace: "helix",
   headers: {
-    'Client-ID': 'e6s7z3s4x3vt4n0adak3hn8ud8ko6c'
+    'Client-ID': 'INSERT YOUR Client-ID HERE'
   },
 
 });
